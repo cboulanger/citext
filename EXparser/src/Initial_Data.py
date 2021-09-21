e7aeec8b6ea53e3de7a6d8ad08fff5176f0f4e42
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2021-09-21 14:09:49 +0200

    Commited out the unpacking that doesn't work

--- EXparser/src/Initial_Data.py
+++ EXparser/src/Initial_Data.py
@@ -1,35 +1,33 @@
 import urllib
-import patoolib
 import shutil
+import os
 
-files=[{'name':'Features',
-        'url':'3szWo6sp3YoAMqo',
-		'in':'./Dataset/'},
-		{'name':'LRT',
-        'url':'HDAXLG6M7ZsaCnT',
-		'in':'./Dataset/'},
-		{'name':'LYT',
-        'url':'zem9C2L3A5Pib3M',
-		'in':'./Dataset/'},
-		{'name':'RefLD',
-        'url':'xLRxqwtiK62pHKy',
-		'in':'./Dataset/'},
-		{'name':'SEG',
-        'url':'RLJsxeRxYz5MY6B',
-		'in':'./Dataset/'},
-		{'name':'Utils',
-        'url':'rSsYdEr2YC6dJSY',
-		'in':'.'}
-		]
-
+files = [{'name': 'Features',
+          'url': '3szWo6sp3YoAMqo',
+          'in': './Dataset/'},
+         {'name': 'LRT',
+          'url': 'HDAXLG6M7ZsaCnT',
+          'in': './Dataset/'},
+         {'name': 'LYT',
+          'url': 'zem9C2L3A5Pib3M',
+          'in': './Dataset/'},
+         {'name': 'RefLD',
+          'url': 'xLRxqwtiK62pHKy',
+          'in': './Dataset/'},
+         {'name': 'SEG',
+          'url': 'RLJsxeRxYz5MY6B',
+          'in': './Dataset/'},
+         {'name': 'Utils',
+          'url': 'rSsYdEr2YC6dJSY',
+          'in': '.'}
+         ]
 
 for file in files:
-	print 'Data being decompressed . . . About 100mb of data will be downloaded . . .'
-	if not os.path.isdir('/app/EXparser/Dataset/'+file['name']):
-		if not os.path.isdir('/app/EXparser/Dataset/Data_Comp'):
-			os.mkdir('/app/EXparser/Dataset/Data_Comp/')
-		url = 'https://cloud.uni-koblenz-landau.de/s/'+file['url']+'/download'
-		urllib.urlretrieve(url, '/app/EXparser/Dataset/Data_Comp/'+file['name']+'.rar')
-		patoolib.extract_archive("/app/EXparser/Dataset/Data_Comp/"+file['name']+".rar", outdir=file['in'])
-shutil.rmtree('/app/EXparser/Dataset/Data_Comp')
-
+    # print 'Data being decompressed . . . About 100mb of data will be downloaded . . .'
+    if not os.path.isdir('../Dataset/' + file['name']):
+        if not os.path.isdir('../Dataset/Data_Comp'):
+            os.mkdir('../Dataset/Data_Comp/')
+        url = 'https://cloud.uni-koblenz-landau.de/s/' + file['url'] + '/download'
+        urllib.urlretrieve(url, '../Dataset/Data_Comp/' + file['name'] + '.rar')
+    # patoolib.extract_archive("/app/EXparser/Dataset/Data_Comp/"+file['name']+".rar", outdir=file['in'])
+shutil.rmtree('../Dataset/Data_Comp')
