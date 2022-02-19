f0f0664cbe47d5a8c2bb6ddc0c219f4d322efd3c
Christian Boulanger <info@bibliograph.org>
2022-02-19 09:46:16 +0100

    Works around #8

--- EXparser/Training_Seg.py
+++ EXparser/Training_Seg.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*- 
 import csv
-import time
 import pickle
 import sklearn_crfsuite
 from .src.gle_fun import *
@@ -23,68 +21,64 @@ def train_segmentation(data_dir: str, model_dir: str):
         fname = os.path.join(fold, curr_file)
         file = open(fname, encoding="utf-8")
         reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)  # , quotechar='|'
+        linenum = 0
         for row in reader:
-            tic = time.time()
-            ln = row[0]
-            if ln.strip() == "":
-                continue
-            ln = re.sub(r'<author>|</author>', '', ln)  # remove author tag
-            ln = re.sub(r'</fpage>|<lpage>', '', ln)  # change page tag
-            ln = re.sub(r'<fpage>', '<page>', ln)  # change page tag
-            ln = re.sub(r'</lpage>', '</page>', ln)  # change page tag
-            ln = preproc(ln)
-            ln = ln.split()
-            l = -1  # no tag is open
+            try:
+                ln = row[0]
+                ln = re.sub(r'<author>|</author>', '', ln)  # remove author tag
+                ln = re.sub(r'</fpage>|<lpage>', '', ln)  # change page tag
+                ln = re.sub(r'<fpage>', '<page>', ln)  # change page tag
+                ln = re.sub(r'</lpage>', '</page>', ln)  # change page tag
+                ln = preproc(ln)
+                ln = ln.split()
+                l = -1  # no tag is open
+                w = ln[0]
+                a, b, l = findtag(w, l)
 
-            w = ln[0]
-            a, b, l = findtag(w, l)
-            train_sents.append([(a, b)])
-            train_feat.append([word2feat(a, stopw, 0, len(ln), b1, b2, b3, b4, b5, b6)])
-            train_label.append([b])
+                train_sents.append([(a, b)])
+                train_feat.append([word2feat(a, stopw, 0, len(ln), b1, b2, b3, b4, b5, b6)])
+                train_label.append([b])
 
-            if 1 < len(ln):
-                w1 = ln[1]
-                a, b, l = findtag(w1, l)
-                train_sents[len(train_sents) - 1].extend([(a, b)])
-                train_feat[len(train_feat) - 1].extend([word2feat(a, stopw, 1, len(ln), b1, b2, b3, b4, b5, b6)])
-                train_label[len(train_label) - 1].extend([b])
-
-            if 2 < len(ln):
-                w2 = ln[2]
-                a, b, l = findtag(w2, l)
-                train_sents[len(train_sents) - 1].extend([(a, b)])
-                train_feat[len(train_feat) - 1].extend([word2feat(a, stopw, 2, len(ln), b1, b2, b3, b4, b5, b6)])
-                train_label[len(train_label) - 1].extend([b])
-            # update features
-            train_feat[len(train_feat) - 1] = add2feat(train_feat[len(train_feat) - 1], 0)
-
-            for i in range(1, len(ln)):
-                # add the +2 word
-                if i < len(ln) - 2:
-                    w = ln[i + 2]
-                    a, b, l = findtag(w, l)
+                if 1 < len(ln):
+                    w1 = ln[1]
+                    a, b, l = findtag(w1, l)
                     train_sents[len(train_sents) - 1].extend([(a, b)])
-                    train_feat[len(train_feat) - 1].extend(
-                        [word2feat(a, stopw, i + 2, len(ln), b1, b2, b3, b4, b5, b6)])
+                    train_feat[len(train_feat) - 1].extend([word2feat(a, stopw, 1, len(ln), b1, b2, b3, b4, b5, b6)])
                     train_label[len(train_label) - 1].extend([b])
-                # add their features to w
+
+                if 2 < len(ln):
+                    w2 = ln[2]
+                    a, b, l = findtag(w2, l)
+                    train_sents[len(train_sents) - 1].extend([(a, b)])
+                    train_feat[len(train_feat) - 1].extend([word2feat(a, stopw, 2, len(ln), b1, b2, b3, b4, b5, b6)])
+                    train_label[len(train_label) - 1].extend([b])
                 # update features
-                train_feat[len(train_feat) - 1] = add2feat(train_feat[len(train_feat) - 1], i)
-            toc = time.time()
-        # print 'processing time = '+ str(toc - tic)
+                train_feat[len(train_feat) - 1] = add2feat(train_feat[len(train_feat) - 1], 0)
+
+                for i in range(1, len(ln)):
+                    # add the +2 word
+                    if i < len(ln) - 2:
+                        w = ln[i + 2]
+                        a, b, l = findtag(w, l)
+                        train_sents[len(train_sents) - 1].extend([(a, b)])
+                        train_feat[len(train_feat) - 1].extend(
+                            [word2feat(a, stopw, i + 2, len(ln), b1, b2, b3, b4, b5, b6)])
+                        train_label[len(train_label) - 1].extend([b])
+                    # add their features to w
+                    # update features
+                    train_feat[len(train_feat) - 1] = add2feat(train_feat[len(train_feat) - 1], i)
+            except IndexError as err:
+                print(curr_file + ", line " + str(linenum) + ": problem parsing " + row[0])
+                continue
+            linenum += 1
         file.close()
-    print("sklearn_crfsuite.CRF")
+    print("Learning, this will take a while...")
     crf = sklearn_crfsuite.CRF(
         algorithm='pa',
         # c2=0.8,
         all_possible_transitions=True,
         all_possible_states=True
     )
-    print("crf.fit")
     crf.fit(train_feat, train_label)
-    print("Saving segmentation model")
     with open(model_dir + '/crf_model.pkl', 'wb') as fid:
         pickle.dump(crf, fid)
-
-
-# train()
