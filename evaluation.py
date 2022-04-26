50b59768a625204529a8cba106fcee725a5f00b6
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2022-04-26 14:06:45 +0200

    Adds file output for evaluation

--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,7 @@ import os
 from enum import Enum
 import re
 from difflib import SequenceMatcher
+from datetime import datetime
 
 
 class Modes(Enum):
@@ -23,36 +24,42 @@ def eval_extraction(gold_folder, out_folder):
 
     Gold and model output's file names should be the same (except for the last extension)."""
 
-    nums = []
-    for file in os.listdir(out_folder):
-        if not file.endswith(".csv"):
-            continue
+    with open("extraction_evaluation_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt", "w") as o:
+        o.write("Gold folder: " + gold_folder + ", Results folder: " + out_folder)
 
-        gold_file_name = file.replace(".csv", ".xml")
-        if os.path.exists(os.path.join(gold_folder, gold_file_name)):
-            with open(os.path.join(out_folder, file)) as in_f, open(os.path.join(gold_folder, gold_file_name)) as gold_f:
-                gold_lines = gold_f.read()
-                # todo: look into other punctuation
-                gold_no_tags = re.sub('<[^<]+>', "", gold_lines).replace(" ", "").replace(",", "").replace(".", "")
+        nums = []
+        for file in os.listdir(out_folder):
+            if not file.endswith(".csv"):
+                continue
 
-                out_lines = in_f.readlines()
+            gold_file_name = file.replace(".csv", ".xml")
+            if os.path.exists(os.path.join(gold_folder, gold_file_name)):
+                with open(os.path.join(out_folder, file)) as in_f, open(os.path.join(gold_folder, gold_file_name)) as gold_f:
+                    gold_lines = gold_f.read()
+                    # todo: look into other punctuation
+                    gold_no_tags = re.sub('<[^<]+>', "", gold_lines).replace(" ", "").replace(",", "").replace(".", "")
 
-                file_nums = []
-                for o_line in out_lines:
-                    o_line_no_spaces = o_line.replace(" ", "").replace(",", "").replace(".", "")
-                    match = SequenceMatcher(None, o_line_no_spaces, gold_no_tags).find_longest_match(0, len(
-                        o_line_no_spaces), 0, len(gold_no_tags))
-                    print(file + ": Longest common sequence with the gold: " + o_line_no_spaces[
-                                                                               match.a:match.a + match.size])
+                    out_lines = in_f.readlines()
 
-                    num = len(o_line_no_spaces[match.a:match.a + match.size]) / len(o_line_no_spaces)
-                    print(str(num))
-                    file_nums.append(num)
-                nums.append(sum(file_nums) / len(file_nums))
-        else:
-            print("The gold file is missing for: " + file)
+                    file_nums = []
+                    for o_line in out_lines:
+                        o_line_no_spaces = o_line.replace(" ", "").replace(",", "").replace(".", "")
+                        match = SequenceMatcher(None, o_line_no_spaces, gold_no_tags).find_longest_match(0, len(
+                            o_line_no_spaces), 0, len(gold_no_tags))
+                        print(file + ": Longest common sequence with the gold: " + o_line_no_spaces[
+                                                                                   match.a:match.a + match.size])
+                        o.write(file + ": Longest common sequence with the gold: " + o_line_no_spaces[
+                                                                                   match.a:match.a + match.size])
 
-    print("Average: " + str(sum(nums) / len(nums)))
+                        num = len(o_line_no_spaces[match.a:match.a + match.size]) / len(o_line_no_spaces)
+                        print(str(num))
+                        file_nums.append(num)
+                    nums.append(sum(file_nums) / len(file_nums))
+            else:
+                print("The gold file is missing for: " + file)
+
+        print("Average: " + str(sum(nums) / len(nums)))
+        o.write("Average: " + str(sum(nums) / len(nums)))
 
 
 tags = ["surname", "given-names", "title", "source", "year", "editor", "publisher", "volume", "issue", "other"]
@@ -77,49 +84,54 @@ def eval_segmentation(gold_folder, out_folder):
     to model output files of the same structure.
     File names should be the same."""
 
-    nums = []
-    for file in os.listdir(out_folder):
-        if not file.endswith(".xml"):
-            continue
+    with open("segmentation_evaluation_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt", "w") as o:
+        o.write("Gold folder: " + gold_folder + ", Results folder: " + out_folder)
 
-        if os.path.exists(os.path.join(gold_folder, file)):
-            with open(os.path.join(out_folder, file)) as in_f, open(os.path.join(gold_folder, file)) as gold_f:
-                # token-to-tags maps
-                out_maps = []
-                for l in in_f:
-                    out_maps.append(get_value_tag_map(l))
+        nums = []
+        for file in os.listdir(out_folder):
+            if not file.endswith(".xml"):
+                continue
 
-                gold_maps = []
-                for l in gold_f:
-                    gold_maps.append(get_value_tag_map(l))
+            if os.path.exists(os.path.join(gold_folder, file)):
+                with open(os.path.join(out_folder, file)) as in_f, open(os.path.join(gold_folder, file)) as gold_f:
+                    # token-to-tags maps
+                    out_maps = []
+                    for l in in_f:
+                        out_maps.append(get_value_tag_map(l))
 
-                if len(out_maps) != len(gold_maps):
-                    print("Segmentation evaluation: different number of lines for: " + file)
-                    print("The file will be skipped")
-                    continue
+                    gold_maps = []
+                    for l in gold_f:
+                        gold_maps.append(get_value_tag_map(l))
 
-                acc_per_line = []
-                for i in range(len(gold_maps)):
-                    out_map = out_maps[i]
-                    gold_map = gold_maps[i]
+                    if len(out_maps) != len(gold_maps):
+                        print("Segmentation evaluation: different number of lines for: " + file)
+                        print("The file will be skipped")
+                        continue
 
-                    correct_value_tag_pairs = 0
-                    gold_value_tag_pairs = len(gold_map.keys())
-                    for k, v in gold_map.items():
-                        if k in out_map:
-                            if v == out_map[k]:
-                                correct_value_tag_pairs += 1
-                    acc_per_line.append(correct_value_tag_pairs / gold_value_tag_pairs)
+                    acc_per_line = []
+                    for i in range(len(gold_maps)):
+                        out_map = out_maps[i]
+                        gold_map = gold_maps[i]
 
-                print("Average accuracy for " + file + ": " + str(sum(acc_per_line) / len(acc_per_line)))
-                nums.append(sum(acc_per_line) / len(acc_per_line))
-        else:
-            print("The gold file is missing for: " + file)
+                        correct_value_tag_pairs = 0
+                        gold_value_tag_pairs = len(gold_map.keys())
+                        for k, v in gold_map.items():
+                            if k in out_map:
+                                if v == out_map[k]:
+                                    correct_value_tag_pairs += 1
+                        acc_per_line.append(correct_value_tag_pairs / gold_value_tag_pairs)
 
-    print("Average for all files: " + str(sum(nums) / len(nums)))
+                    print("Average accuracy for " + file + ": " + str(sum(acc_per_line) / len(acc_per_line)))
+                    o.write("Average accuracy for " + file + ": " + str(sum(acc_per_line) / len(acc_per_line)))
+                    nums.append(sum(acc_per_line) / len(acc_per_line))
+            else:
+                print("The gold file is missing for: " + file)
+
+        print("Average for all files: " + str(sum(nums) / len(nums)))
+        o.write("Average for all files: " + str(sum(nums) / len(nums)))
 
 
 # for test
 if __name__ == '__main__':
-    # compare_output_to_gold("Data/test/extract/gold/", "Data/test/extract/", Modes.EXTRACTION.value)
-    compare_output_to_gold("Data/test/segment/gold/", "Data/test/segment/", Modes.SEGMENTATION.value)
+    compare_output_to_gold("Data/test/extract/gold/", "Data/test/extract/", Modes.EXTRACTION.value)
+    # compare_output_to_gold("Data/test/segment/gold/", "Data/test/segment/", Modes.SEGMENTATION.value)
