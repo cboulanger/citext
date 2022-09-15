249c88763b0ba8e9e0cb3dc9b3bfb29dd36f03b2
Christian Boulanger <info@bibliograph.org>
2022-09-15 15:04:32 +0200

    Check for existing file

--- cgi-bin/find.rb
+++ cgi-bin/find.rb
@@ -9,15 +9,19 @@ file_name = cgi.params["filename"].first
 model_name = cgi.params["model"].first
 file_path = File.join("tmp", file_name).untaint
 
-# find refs in document
-if model_name != "default"
-    AnyStyle.finder.load_model File.join("Models", model_name, "finder.mod").untaint
+if ! File.exists?(file_path)
+    response = {error: "File #{file_name} cannot be found."}
+else
+    # find refs in document
+    if model_name != "default"
+        AnyStyle.finder.load_model File.join("Models", model_name, "finder.mod").untaint
+    end
+    response = AnyStyle.finder.find(file_path, format: :wapiti)[0]
+        .to_s(tagged:true)
+        .gsub(/Generiert durch Max\-Planck\-Institut für Rechtsgeschichte und Rechtstheorie, am ([^,]+), ([^.]+)\./,"")
+        .gsub(/Das Erstellen und Weitergeben von Kopien dieses PDFs ist nicht zulässig\./,"")
+        .gsub(/https:\/\/doi\.org\/[\d.\/_]+/,"")
 end
-ttx = AnyStyle.finder.find(file_path, format: :wapiti)[0]
-    .to_s(tagged:true)
-    .gsub(/Generiert durch Max\-Planck\-Institut für Rechtsgeschichte und Rechtstheorie, am ([^,]+), ([^.]+)\./,"")
-    .gsub(/Das Erstellen und Weitergeben von Kopien dieses PDFs ist nicht zulässig\./,"")
-    .gsub(/https:\/\/doi\.org\/[\d.\/_]+/,"")
 
 # return to client
-cgi.out("application/json") { ttx.to_json }
+cgi.out("application/json") { response.to_json }
