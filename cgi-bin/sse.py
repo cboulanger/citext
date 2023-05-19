cb358cc13515d2219faac0ca70b54ad1b0053b5d
Christian Boulanger <info@bibliograph.org>
2023-05-19 22:06:52 +0200

    use sqlite db instead of files to keep track of sse sessions

--- cgi-bin/sse.py
+++ cgi-bin/sse.py
@@ -1,61 +1,105 @@
 #!/usr/bin/env python3
 import re, sys, time, os, json, traceback, socket, errno
+import sqlite3
+from datetime import datetime, timedelta
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from configs import *
 
+# a random integer id for the channel for which this process acts as a SSE backend
 channel_id = os.environ.get('QUERY_STRING')
 
+# SSE protocol output
 sys.stdout.write('Cache-Control: no-store\n')
 sys.stdout.write('Content-type: text/event-stream\n\n')
 
-# cleanup channel_id -> port cache
-for file_name in os.listdir(config_tmp_dir()):
-    if not re.match("^[\d]+$", file_name): continue
-    file_path = os.path.join(config_tmp_dir(), file_name)
-    with open(file_path) as f:
-        port = int(f.read())
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.bind(('', port))
-            os.remove(file_path)
-            if os.path.exists(file_path + ".abort"):
-                os.remove(file_path +".abort")
-        except socket.error as e:
-            if e.errno != errno.EADDRINUSE:
-                pass
-            else:
-                print(e)
-        s.close()
+# connect to SQLite database
+sock_conn = sqlite3.connect('tmp/ports.db')
+cursor = sock_conn.cursor()
 
-# bind to new socket and save associated port
+# create table if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS channels
+    (id INTEGER PRIMARY KEY, port INTEGER NOT NULL, last_action TIMESTAMP NOT NULL);
+''')
+
+# bind to new socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('', 0))
 port = sock.getsockname()[1]
 sock.listen(5)
-with open(os.path.join(config_tmp_dir(), channel_id), "w") as f:
-    f.write(str(port))
 
+# save associated port in the sqlite3 database so that the port is
+# discoverable by other processes that know the channel id
+
+# check if the channel_id already exists
+cursor.execute('SELECT * FROM channels WHERE id=?', (channel_id,))
+row = cursor.fetchone()
+
+# insert or update the port and the last_action timestamp in the database
+if row is None:
+    cursor.execute('''
+        INSERT INTO channels(id, port, last_action)
+        VALUES (?, ?, ?);
+    ''', (channel_id, port, datetime.now()))
+else:
+    cursor.execute('''
+        UPDATE channels
+        SET port = ?, last_action = ?
+        WHERE id = ?;
+    ''', (port, datetime.now(), channel_id))
+
+# commit changes
+sock_conn.commit()
+
+# terminate processes which haven't been used in more than an hour
+one_hour_ago = datetime.now() - timedelta(hours=1)
+cursor.execute('SELECT port FROM channels WHERE last_action < ?', (one_hour_ago,))
+rows = cursor.fetchall()
+for row in rows:
+    event_json = json.dumps({
+        "name": "sse_terminate",
+        "data": ""
+    })
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect(('', row[0]))
+        client.send(bytes(event_json, 'utf-8'))
+    finally:
+        client.close()
+
+sock_conn.close()
+
+# log output
 sys.stderr.write(f"SSE: Connected channel id {channel_id} with port {port}\n")
 sys.stdout.write(f"event:debug\ndata:Server connected with channel id {channel_id}\n\n")
 sys.stdout.flush()
 
+# start infinite loop that listens for incoming messages on the given port
 while True:
-    conn, addr = sock.accept()
+    sock_conn, addr = sock.accept()
     try:
         from_client = ''
         while True:
-            data = conn.recv(4096).decode()
+            data = sock_conn.recv(4096).decode()
             if not data: break
-            #sys.stderr.write(f"Received as str: {data}\n")
             from_client += data
         evt = json.loads(from_client)
         event_name = evt['name']
         event_data = evt['data']
+        # Terminate process when receiving the "sse_terminate" event
         if event_name == "sse_terminate":
-            conn.close()
+            # remove entry in database
+            sql_conn = sqlite3.connect('tmp/ports.db')
+            cursor = sql_conn.cursor()
+            cursor.execute('DELETE FROM channels WHERE id = ?', (channel_id,))
+            sql_conn.commit()
+            sql_conn.close()
+            # close port listener
+            sock_conn.close()
             message = f"SSE: Shutting down process for channel #{channel_id}.\n"
             sys.stderr.write(message)
             sys.exit(0)
+        # pass event data to client
         sys.stdout.write(f"event:{event_name}\ndata:{event_data}\n\n")
         sys.stdout.flush()
 
@@ -66,5 +110,4 @@ while True:
         sys.stdout.flush()
 
     finally:
-        conn.close()
-
+        sock_conn.close()
--- lib/sse.py
+++ lib/sse.py
@@ -2,17 +2,30 @@ import json
 import os, sys, subprocess, re, builtins
 import socket
 from configs import *
-from lib.logger import log
+import sqlite3
 
 class SSE:
     def __init__(self, channel_id):
         self.channel_id = channel_id
 
-    def push_event(self, event_name: str, event_data: str = ""):
         # find out which internal port this channel id is associated with
-        file_path = os.path.join(config_tmp_dir(), str(self.channel_id))
-        with open(file_path) as f:
-            port = int(f.read())
+        conn = sqlite3.connect('tmp/ports.db')
+        cursor = conn.cursor()
+
+        # Retrieve port for a given channel_id
+        cursor.execute('SELECT port FROM channels WHERE id=?', (channel_id,))
+        port_tuple = cursor.fetchone()
+        if port_tuple is not None:
+            port = port_tuple[0]
+        else:
+            raise RuntimeError(f"No port exists for channel id {channel_id}")
+
+        self.port = port
+        conn.close()
+
+
+    def push_event(self, event_name: str, event_data: str = ""):
+
         # sys.stderr.write(f"Connecting to channel {channel_id} via port {str(port)}...\n")
         event_json = json.dumps({
             "name": event_name,
@@ -21,7 +34,7 @@ class SSE:
         # sys.stderr.write(f"To client: {event_json}\n")
         # connect to event server and send event
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(('', port))
+        client.connect(('', self.port))
         client.send(bytes(event_json, 'utf-8'))
         client.close()
         return True
